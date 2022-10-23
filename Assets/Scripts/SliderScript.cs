231b17d5ea6a7bef92222872fb923f1387d35b8b
Mrmaraunet <aramlomax@free.fr>
2022-10-23 17:22:04 +0200

    manual input is designer

--- Assets/Scripts/SliderScript.cs
+++ Assets/Scripts/SliderScript.cs
@@ -1,4 +1,3 @@
-using System.Xml;
 using Assets.Scripts;
 using ModApi.Ui;
 using TMPro;
@@ -9,19 +8,35 @@ public class SliderScript : MonoBehaviour
 {
     private Slider _fovSlider;
     public IXmlLayout xmlLayout;
-    private TextMeshProUGUI textValue;
+    private TextMeshProUGUI valueText;
     public void OnLayoutRebuilt(IXmlLayout xmlLayout)
     {
         this.xmlLayout = xmlLayout;
         _fovSlider = xmlLayout.GetElementById<Slider>("fov-slider");
-        textValue = xmlLayout.GetElementById<TextMeshProUGUI>("fov-slider-text");
+        valueText = xmlLayout.GetElementById<TextMeshProUGUI>("fov-slider-text");
 
         _fovSlider.onValueChanged.AddListener(Mod.Instance.OnDesignerFovSliderEdit);
-        _fovSlider.onValueChanged.AddListener(delegate (float value) { textValue.text = $"{value:N0}"; });
+        _fovSlider.onValueChanged.AddListener(delegate (float value) { valueText.text = $"{value:N0}"; });
+        xmlLayout.GetElementById("fov-slider-text").AddOnClickEvent(delegate
+            {
+                Debug.Log("Value Clicked");
+                ModApi.Ui.InputDialogScript dialog = Game.Instance.UserInterface.CreateInputDialog();
+                dialog.MessageText = "Enter Value";
+                dialog.InputText = valueText.text;
+                dialog.OkayClicked += delegate (ModApi.Ui.InputDialogScript d)
+                {
+                    d.Close();
+                    Debug.Log(d.InputText);
+                    if (float.TryParse(d.InputText, out var result))
+                    {
+                        _fovSlider.value = result;
+                    }
+                };
+            });
 
         _fovSlider.minValue = 5f;
         _fovSlider.maxValue = 160f;
         _fovSlider.value = Game.Instance.Settings.Game.General.FieldOfView;
-        textValue.text = $"{Game.Instance.Settings.Game.General.FieldOfView:N0}";
+        valueText.SetText($"{Game.Instance.Settings.Game.General.FieldOfView:N0}");
     }
 }
